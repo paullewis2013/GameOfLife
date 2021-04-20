1b20dc2e290534af6899ffee43d4265f2f542662
paullewis2013 <paullewis2013@gmail.com>
2021-04-20 15:55:51 -0400

    Layout overhaul

--- CanvasMain.js
+++ CanvasMain.js
@@ -25,29 +25,6 @@ const light3 = new THREE.HemisphereLight(skyColor, groundColor, intensity);
 scene.add(light3);
 
 
-var cubeArr = [];
-var projections = [];
-var lifeMain = new Life2D(48)
-
-for(let i = 0; i < 48; i++){
-
-    cubeArr[i] = []
-
-    for(let j = 0; j < 48; j++){
-
-        let tempColor = randColor()
-        let tempMat = new THREE.MeshBasicMaterial( { color: tempColor} );
-
-        let tempCube = new THREE.Mesh(geometry, tempMat);
-        tempCube.position.x = -24 + i
-        tempCube.position.z = -24 + j
-
-        tempCube.position.y =  1 
-        cubeArr[i][j] = tempCube
-        scene.add(cubeArr[i][j])
-    }
-}
-
 function randColor(){
 
     colors = [
@@ -63,10 +40,62 @@ function randColor(){
 
     return colors[Math.floor(Math.random() * colors.length)]
 }
+function tint(c1, w){
+
+    // console.log(c1)
+
+    let r = parseInt(c1.substring(0,2), 16)
+    let g = parseInt(c1.substring(2,4), 16)
+    let b = parseInt(c1.substring(4), 16)
+
+    // console.log(r, g, b)
+
+    r = Math.floor(r + (w * (255 - r)))
+    g = Math.floor(g + (w * (255 - g)))
+    b = Math.floor(b + (w * (255 - b)))
+
+    r = r.toString(16)
+    g = g.toString(16)
+    b = b.toString(16)
+
+    num = "" + r + g + b
+
+    return parseInt(num, 16)
+
+}
 
 var frameNum = 0
 var currColer = 0xb58900
 var projectionsCounter = 0;
+
+var lifeSize = 64
+
+lifeMain = new Life2D(lifeSize)
+currColer = randColor()
+lifeMain.randomize(0.15)
+
+var cubeArr = [];
+var projections = [];
+// var lifeMain = new Life2D(48)
+
+for(let i = 0; i < lifeSize; i++){
+
+    cubeArr[i] = []
+
+    for(let j = 0; j < lifeSize; j++){
+
+        let tempColor = randColor()
+        let tempMat = new THREE.MeshBasicMaterial( { color: tempColor} );
+
+        let tempCube = new THREE.Mesh(geometry, tempMat);
+        tempCube.position.x = -lifeSize/2 + i
+        tempCube.position.z = -lifeSize/2 + j
+
+        tempCube.position.y =  1 
+        cubeArr[i][j] = tempCube
+        scene.add(cubeArr[i][j])
+    }
+}
 
 const animate = function () {
 
@@ -75,10 +104,6 @@ const animate = function () {
 
     //reset base every 48 frames
     if(frameNum % 48 == 1){
-
-        lifeMain = new Life2D(48)
-        currColer = randColor()
-        lifeMain.randomize(0.15)
 
         //loop through base
         for(let i = 0; i < cubeArr.length; i++){
@@ -115,7 +140,7 @@ const animate = function () {
         projections.push(Array.from(lifeMain.state))
         lifeMain.update()
 
-        let tempColor = tint(currColer.toString(16), projectionsCounter/48)
+        let tempColor = tint(currColer.toString(16), projectionsCounter/lifeSize)
         // console.log(tempColor)
         let tempMatDraw = new THREE.MeshBasicMaterial( { color: 0xffffff, wireframe: false} );
 
@@ -126,8 +151,8 @@ const animate = function () {
                 if(projections[projections.length-1][row][col]){
 
                     let tempCube = new THREE.Mesh(geometry, tempMatDraw);
-                    tempCube.position.x = -24 + row
-                    tempCube.position.z = -24 + col
+                    tempCube.position.x = -lifeSize/2 + row
+                    tempCube.position.z = -lifeSize/2 + col
 
                     tempCube.position.y =  projections.length 
                     projections[projections.length-1][row][col] = tempCube
@@ -156,31 +181,9 @@ const animate = function () {
     }
 
     renderer.render( scene, camera );
+    drawFrame1()
 };
 
 setInterval(animate, 100);
 // animate();
 
-function tint(c1, w){
-
-    console.log(c1)
-
-    let r = parseInt(c1.substring(0,2), 16)
-    let g = parseInt(c1.substring(2,4), 16)
-    let b = parseInt(c1.substring(4), 16)
-
-    console.log(r, g, b)
-
-    r = Math.floor(r + (w * (255 - r)))
-    g = Math.floor(g + (w * (255 - g)))
-    b = Math.floor(b + (w * (255 - b)))
-
-    r = r.toString(16)
-    g = g.toString(16)
-    b = b.toString(16)
-
-    num = "" + r + g + b
-
-    return parseInt(num, 16)
-
-}
--- canvas1.js
+++ canvas1.js
@@ -1,6 +1,6 @@
 var canvas = document.getElementById("canvas1")
 
-canvas.style.height = window.innerHeight/3 + "px";  
+canvas.style.height = window.innerHeight * 33/100 + "px";  
 canvas.style.width = canvas.style.height
 
 var ctx1 = canvas.getContext('2d')
@@ -10,24 +10,24 @@ var scale = window.devicePixelRatio; // Change to 1 on retina screens to see blu
 canvas.height = Math.floor((window.innerHeight/3) * scale);
 canvas.width = canvas.height;
 
-var life1 = new Life2D(40)
-life1.randomize(0.5)
+// var life1 = new Life2D(48)
+// life1.randomize(0.5)
 
 function drawFrame1(){
-    life1.update()
+    // life1.update()
     
-    let h = canvas.height/(life1.state.length * 1.55);
+    let h = canvas.height/(lifeMain.state.length );
 
-    for(let row = 0; row < life1.state.length; row++){
-        for(let col = 0; col < life1.state[row].length; col++){
+    for(let row = 0; row < lifeMain.state.length; row++){
+        for(let col = 0; col < lifeMain.state[row].length; col++){
             
             ctx1.beginPath();
             ctx1.rect(col*h, row*h, h, h);
             
-            if(life1.state[row][col]){
-                ctx1.fillStyle = "#6c71c4";
+            if(lifeMain.state[col][row]){
+                ctx1.fillStyle = "white";
             }else{
-                ctx1.fillStyle = "black";
+                ctx1.fillStyle = "#" + currColer.toString(16);
             }
             ctx1.fill();
 
@@ -36,5 +36,5 @@ function drawFrame1(){
 }
 
 
-setInterval(drawFrame1, 150);
+// setInterval(drawFrame1, 150);
 
